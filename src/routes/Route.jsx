dfd3743e4daa7e91b0448b1d97ab90d3f5c9c091
smais007 <smaisshawon@gmail.com>
2024-06-11 14:50:12 +0600

    login paged and home page added

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,8 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home";
+import Login from "../components/Login";
+import Root from "../layouts/Root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
